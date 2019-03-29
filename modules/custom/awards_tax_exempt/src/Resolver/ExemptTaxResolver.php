9ca7ea4de6da0a4b0adf57a854aafd7743bea12d
Shawn Ostermann <shawn@ccctampabay.com>
2019-03-29 11:03:10 -0400

    Bug fix for Bill Later module

--- modules/custom/awards_tax_exempt/src/Resolver/ExemptTaxResolver.php
+++ modules/custom/awards_tax_exempt/src/Resolver/ExemptTaxResolver.php
@@ -8,23 +8,58 @@ use Drupal\commerce_order\Entity\OrderItemInterface;
 use Drupal\commerce_tax\Resolver\TaxRateResolverInterface;
 use Drupal\commerce_tax\TaxZone;
 use Drupal\profile\Entity\ProfileInterface;
-
+use Drupal\commerce_tax\TaxRate;
 
 /**
  * Returns the tax zone's default tax rate.
  */
-class ExemptTaxResolver implements TaxRateResolverInterface {
+class ExemptTaxResolver implements TaxRateResolverInterface
+{
 
   /**
    * {@inheritdoc}
    */
-  public function resolve(TaxZone $zone, OrderItemInterface $order_item, ProfileInterface $customer_profile) {
+  public function resolve(TaxZone $zone, OrderItemInterface $order_item, ProfileInterface $customer_profile)
+  {
+    // commerce_shipment
+    // 4 & 5
+    $order = \Drupal\commerce_order\Entity\Order::load($order_item->getOrderId());
+    //$shipments = $order->get('shipments')->getValue();
 
-    // If no rate has been found, let's others resolvers try to get it.
-    if (\Drupal\user\Entity\User::load(\Drupal::currentUser()->id())->hasRole('tax_exempt')){
-      return TaxRateResolverInterface::NO_APPLICABLE_TAX_RATE;
+    $rate = new TaxRate(
+      ['id' => 'pst',
+        'label' => 'PST',
+        'percentages' => [
+          ['number' => '0.07', 'start_date' => '2013-04-01'],
+        ],
+        'default' => TRUE,
+      ]
+    );
+    dpm($rate);
+    return $rate;
+
+    foreach ($order->shipments as $reference) {
+
+      $method_id = $reference->entity->getShippingMethodId();
+      $rate = new TaxRate(
+        ['id' => 'pst',
+          'label' => 'PST',
+          'percentages' => [
+            ['number' => '0.07', 'start_date' => '2013-04-01'],
+          ],
+          'default' => TRUE,
+        ]
+      );
+      //dpm($rate);
+
+      return $rate;
+
+
+      // If no rate has been found, let's others resolvers try to get it.
+      if (\Drupal\user\Entity\User::load(\Drupal::currentUser()->id())->hasRole('tax_exempt')){
+        return TaxRateResolverInterface::NO_APPLICABLE_TAX_RATE;
+      }
+
     }
-
   }
-
 }
