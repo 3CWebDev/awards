ba48826725676902bd1f1469a7888025e99176f7
Shawn Ostermann <shawn@ccctampabay.com>
2019-05-22 21:25:02 -0400

    Changed Price Breaks module to use order processor instead of price resolver so that carts with multiple line items of the same SKU would be counted together.

---
+++ modules/custom/awards_price_breaks/src/CustomOrderProcessor.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Drupal\awards_price_breaks;
+
+use Drupal\commerce_order\Entity\OrderInterface;
+use Drupal\commerce_order\OrderProcessorInterface;
+use Drupal\commerce_price\Price;
+use Drupal\commerce_product\Entity\Product;
+use Drupal\commerce_order\Adjustment;
+
+/**
+ * Provides an order processor that modifies the cart according to the business logic.
+ */
+class CustomOrderProcessor implements OrderProcessorInterface
+{
+  /**
+   * {@inheritdoc}
+   */
+  public function process(OrderInterface $order)  {
+
+    $total_quantity = 0;
+    foreach ($order->getItems() as $order_item) {
+      $product_variation = $order_item->getPurchasedEntity();
+      $sku_main = $product_variation->getSku();
+      foreach ($order->getItems() as $order_item) {
+        $product_variation = $order_item->getPurchasedEntity();
+        if (isset($product_variation->field_prod_var_price_break) && !$product_variation->field_prod_var_price_break->isEmpty()) {
+          $sku = $product_variation->getSku();
+          if ($sku == $sku_main) {
+            $total_quantity = $total_quantity + $order_item->quantity->getString();
+          }
+
+          foreach ($product_variation->field_prod_var_price_break as $price_break) {
+            if ($total_quantity >= intval($price_break->threshold)) {
+
+              if (!isset($current_pricebreak) || $current_pricebreak->threshold < $price_break->threshold) {
+                $current_pricebreak = $price_break;
+              }
+            }
+          }
+
+          if (isset($current_pricebreak)) {
+            $price = new Price($current_pricebreak->price, 'USD');
+            $order_item->setUnitPrice($price);
+          }
+        }
+      }
+    }
+
+  }
+}
--- modules/custom/awards_price_breaks/src/Resolvers/QtyPriceResolver.php
+++ modules/custom/awards_price_breaks/src/Resolvers/QtyPriceResolver.php
@@ -13,9 +13,37 @@ use Drupal\commerce_price\Price;
 class QtyPriceResolver implements PriceResolverInterface {
 
   public function resolve(PurchasableEntityInterface $entity, $quantity, Context $context) {
+
+    $store_id = 1;
+    $order_type = 'default';
+    //$cart_manager = \Drupal::service('commerce_cart.cart_manager');
+    $cart_provider = \Drupal::service('commerce_cart.cart_provider');
+    $entity_manager = \Drupal::entityManager();
+    $store = $entity_manager->getStorage('commerce_store')->load($store_id);
+    $cart = $cart_provider->getCart($order_type, $store);
+
+    if (!$cart){
+      return;
+    }
+
+    $items = $cart->getItems();
+
+    $total_quantity = 0;
+    $sku_main = $entity->getSku();
+
+
     if (isset($entity->field_prod_var_price_break) && !$entity->field_prod_var_price_break->isEmpty()) {
+
+      foreach ($items as $item){
+        $product_variation = $item->getPurchasedEntity();
+        $sku = $product_variation->getSku();
+        if ($sku == $sku_main){
+          $total_quantity = $total_quantity + $item->quantity->getString();
+        }
+      }
+
       foreach ($entity->field_prod_var_price_break as $price_break) {
-        if ($quantity >= intval($price_break->threshold)) {
+        if ($total_quantity >= intval($price_break->threshold)) {
           if (!isset($current_pricebreak) || $current_pricebreak->threshold < $price_break->threshold) {
             $current_pricebreak = $price_break;
           }
@@ -26,6 +54,9 @@ class QtyPriceResolver implements PriceResolverInterface {
         return new Price($current_pricebreak->price, 'USD');
       }
     }
+
+
   }
+
 
 }
