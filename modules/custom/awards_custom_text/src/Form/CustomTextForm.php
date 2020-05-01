dc2ccd1b67025dae327a5886f8f0e8fb535ad109
3CWebDev <shawn@ccctampabay.com>
2020-05-01 09:48:37 -0400

    Fixed empty cart issue.

--- modules/custom/awards_custom_text/src/Form/CustomTextForm.php
+++ modules/custom/awards_custom_text/src/Form/CustomTextForm.php
@@ -12,7 +12,8 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 use Drupal\image\Entity\ImageStyle;
 use Drupal\file\Entity\File;
 use Drupal\Component\Utility\SafeMarkup;
-//use Drupal\commerce_cart\CartProviderInterfac;
+use Drupal\Core\Url;
+
 /**
  * Contribute form.
  */
@@ -43,6 +44,7 @@ class CustomTextForm extends FormBase {
     $all_carts = \Drupal::service('commerce_cart.cart_provider')
       ->getCarts();
     $first_cart = reset($all_carts);
+
     $order_items = $first_cart->order_items->getValue();
     $verified = FALSE;
     foreach ($order_items as $key => $order_item){
@@ -468,7 +470,10 @@ class CustomTextForm extends FormBase {
     $order_item->save();
     $order = \Drupal\commerce_order\Entity\Order::load($values['order_id']);
     $order->save();
+    //$form['#cache'] = ['max-age' => 0];
     $response = new RedirectResponse('/cart', 302);
+    //$url = Url::fromRoute('commerce_cart.page');
+    //$form_state->setRedirectUrl($url);
     $response->send();
   }
   public function callback(array &$form, FormStateInterface $form_state) : array {
